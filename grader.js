b64ecf06a8deb7c5e1d4453d5c965ff43f185b80
Wilson Rondini <wilbecoding@gmail.com>
2015-08-09 18:43:07 -0600

    Need to fix Syntax

--- grader.js
+++ grader.js
@@ -1,27 +1,68 @@
-var Score = {
-  letterGrader: function(score) {
+var score = {
 
-    if(score > 89) {
-      return "A";
-    }
+letterGrader: function(grade) {
+  if(grade > 89) {
+    return "A";
+  }
+  if(grade > 79 && grade < 90) {
+    return "B";
+  }
 
-    if(score > 79 && score < 90) {
-      return "B";
-    }
+  if(grade > 69 && grade < 80) {
+    return "C";
+  }
 
-    if(score > 69 && score < 80) {
-      return "C";
-    }
+  if(grade > 59 && grade < 70) {
+    return "D";
+  }
 
-    if(score > 59 && score < 70) {
-      return "D";
-    }
-
-    if(score < 60) {
-      return "F";
-    }
-
+  if(grade < 60) {
+    return "F";
   }
 }
 
-module.exports = Score;
+
+averageScore: function(average) {
+  var sum = 0;
+    for (var i = 0; i < average.length; i++) {
+    sum += average[i]
+    } 
+  return sum / average.length;
+};
+
+medianScore: function(median) {
+median.sort();
+var half = Math.floor(median.length / 2);
+
+if ((median.length / 2) % 2 === 0) {
+return (median[half]);
+}
+else if ((median.length /2) % 2 !== 0) {
+  return (median[half -1] + median[half]) / 2;
+}
+};
+
+modeScore: function(mode) {
+    var counter = {};
+    var mode = [];
+    var max = 0;
+    for (var i in mode) {
+        if (!(mode[i] in counter))
+            counter[mode[i]] = 0;
+        counter[mode[i]]++;
+ 
+        if (counter[mode[i]] == max) 
+            mode.push(mode[i]);
+        else if (counter[mode[i]] > max) {
+            max = counter[mode[i]];
+            mode = [mode[i]];
+        }
+    }
+    return mode; 
+}
+
+
+
+}
+
+module.exports = score;
--- spec/grader_spec.js
+++ spec/grader_spec.js
@@ -1,29 +1,57 @@
-var Score = require('../house');
+var score = require('../grader.js');
 
-describe('Score', function() {
+  describe('methods', function() {
+    describe('score', function() {
+    it('uses the grade argument as a property', function() {
+      // var score = new score(100);
+
+      expect(score.grade(100)).toEqual('A');
+    });
+
+    it('Returns a B letterGrader for a score between 80 and 89', function() {
+        expect(score.grade(87)).toEqual('B');
+    });
+
+    it('Returns a C letterGrader for a score between 70 and 79', function() {
+
+        expect(score.grade(77)).toEqual('C');
+    });
+
+    it('Returns a D letterGrader for a score between 60 and 69', function() {
   
-  it('Returns an A letterGrader for a score between 90 and 100', function() {
-    var score = new Score(93);
-      expect(Score.letterGrader()).toEqual('A');
+        expect(score.grade(67)).toEqual('D');
+    });
+
+    it('Returns an F letterGrader for a score below 60', function() {
+    
+        expect(score.grade(12)).toEqual('F');
+    });
   });
 
-  it('Returns a B letterGrader for a score between 80 and 89', function() {
-    var score = new Score(87);
-      expect(Score.letterGrader()).toEqual('B');
+  describe('methods', function() {
+    describe('averageScore', function() {
+
+      it('Takes the grades in an array and returns the average grade', function() {
+          expect(score.average([76,89,53,100,96])).toEqual(82.8);
+      });
+    });
   });
 
-  it('Returns a C letterGrader for a score between 70 and 79', function() {
-    var score = new Score(73);
-      expect(Score.letterGrader()).toEqual('C');
+  descibe('methods', function() {
+    describe('medianScore' function() {
+
+      it('Takes the grades of the students and finds the median', function() {
+        expect(score.median([52,80,80,86,94])).toEqual(80);
+      });
+    });
   });
 
-  it('Returns a D letterGrader for a score between 60 and 69', function() {
-    var score = new Score(66);
-      expect(Score.letterGrader()).toEqual('D');
-  });
+  describe('methods', function() {
+    describe('modeSCore' function() {
 
-  it('Returns an F letterGrader for a score below 60', function() {
-    var score = new Score(59);
-      expect(Score.letterGrader()).toEqual('F');
+      it('Takes the grades of the students and finds the mode', function() {
+        expect(score.mode([82, 92, 75, 91, 92, 89, 95, 100, 86])).toEqual(92);
+      });
+    });
   });
-}
+});
